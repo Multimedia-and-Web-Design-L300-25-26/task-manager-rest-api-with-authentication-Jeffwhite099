2dfa3844e530705acf53f0d805dd491f0a3c3b50
jeffwhite099 <jephthahpeprah099@gmai.coml>
2026-03-07 10:24:57 +0000

    Implement authentication middleware, user and task models, and update Jest configuration

--- tests/setup.js
+++ tests/setup.js
@@ -1,3 +1,23 @@
-import app from "../src/app.js";
+import { MongoMemoryServer } from "mongodb-memory-server";
+import mongoose from "mongoose";
 
-export default app;
+let mongoServer;
+
+beforeAll(async () => {
+  process.env.JWT_SECRET = "test_secret";
+  mongoServer = await MongoMemoryServer.create();
+  const uri = mongoServer.getUri();
+  await mongoose.connect(uri);
+});
+
+afterAll(async () => {
+  await mongoose.disconnect();
+  await mongoServer.stop();
+});
+
+afterEach(async () => {
+  const collections = mongoose.connection.collections;
+  for (const key in collections) {
+    await collections[key].deleteMany({});
+  }
+});
